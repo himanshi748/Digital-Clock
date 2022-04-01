b262d83578a6436a8314641b49e2b82ae3239280
Himanshi Satpute <57482731+himanshi748@users.noreply.github.com>
2022-04-01 23:55:18 +0530

    clock

---
+++ digital clock/p41.js
@@ -0,0 +1,96 @@
+function clock(){
+
+    var hours = document.getElementById('hour');
+    var minutes = document.getElementById('min');
+    var seconds = document.getElementById('sec');
+    var am = document.getElementById('ampm');
+
+    var time = new Date();
+
+    var h = time.getHours();
+    var m = time.getMinutes();
+    var s = time.getSeconds();
+    var a = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        a = "PM";
+    }
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (s < 10) {
+        s = "0" + s;
+    }
+    hours.innerText = h;
+    minutes.innerText = m;
+    seconds.innerText = s;
+    am.innerText = a;
+}
+setInterval(clock,1000);
+
+function change(){
+    let result1 = document.getElementById('morning');
+    let result2 = result1.value;
+    document.getElementById('wake').innerHTML=result2;
+ 
+    let result3 = document.getElementById('afternoon');
+    let result4 = result3.value;
+    document.getElementById('lunch').innerHTML=result4;
+
+    let result5 = document.getElementById('evening');
+    let result6 = result5.value;
+    document.getElementById('nap').innerHTML=result6;
+
+    let result7 = document.getElementById('night');
+    let result8 = result7.value;
+    document.getElementById('nighttime').innerHTML=result8; 
+}
+
+function settime(){
+
+    var a =document.getElementById('morning').value;
+    var b = document.getElementById('afternoon').value;
+    var c = document.getElementById("evening").value;
+    var d = document.getElementById('night').value;
+    
+    var h = new Date().getHours();
+
+    if(a == h){
+
+        document.getElementById('imgcontainer').style.backgroundImage="url(wakeup.png)"
+        document.getElementById('health').innerHTML='Wake Up !!';
+        document.getElementById("gm").innerHTML="Good Morning !!";
+    }
+
+    if(b == h){
+        document.getElementById("imgcontainer").style.backgroundImage="url(lunch.png)"
+        document.getElementById("health").innerHTML="Let's Have Some Lunch !!";
+        document.getElementById("gm").innerHTML="Good Afternoon !!";
+    }
+
+    if(c ==h ){
+        document.getElementById("imgcontainer").style.backgroundImage="url(defaul.png)"
+        document.getElementById("health").innerHTML="Let's Have Some Dinner !!";
+        document.getElementById("gm").innerHTML="Good Evening !!";
+    } 
+    if(d == h){
+        document.getElementById("imgcontainer").style.backgroundImage="url(night.png)"
+        document.getElementById("health").innerHTML="Good Night !!";
+        document.getElementById("gm").innerHTML="Good Night !!";
+    }
+    change();
+}
+
+document.getElementById("imgcontainer").style.backgroundImage="url(./wakeup.PNG)";
+document.getElementById('health').innerHTML='GRAB SOME HEALTHY BREAKFAST!!!';
+document.getElementById('gm').innerHTML='GOOD MORNING!!WAKE UP!!';
+
+
